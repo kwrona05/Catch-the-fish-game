304b06a614397df3888d562463543bb9eebde9e3
kwrona05 <kwrona05@gmail.com>
2024-08-23 10:41:17 +0200

    Create fishing feature to loot random item

--- catch_fish/src/App.jsx
+++ catch_fish/src/App.jsx
@@ -1,8 +1,35 @@
+import React, { useState } from "react"
 
 function App() {
+  const fish1 = ["Salmon", "Pufferfish", "Cod","Heart_of_the_sea", "Gold", "Shoe"]
+  
+  const [randomFish, setRandomFish] = useState(null)
+  const [items, setItems] = useState([])
+
+  const handleRandomize = () => {
+    const randomIndex = Math.floor(Math.random() * fish1.length)
+    const newItem = fish1[randomIndex]
+    
+    setRandomFish(newItem)
+
+    setItems(prevItems => [...prevItems, newItem])
+  }
+
   return(
 <div className="main">
-  <h1>Fishy Party</h1>
+  <div className="sea">
+    <h1>Fishy Party</h1>
+    <button className="fishing" onClick={handleRandomize}>Fishing</button>
+    {randomFish && <p>Your loot: {randomFish}</p>}
+  </div>
+    <div className="equipment">
+      <h3>Your equipment:</h3>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
 </div>
   )
 }
